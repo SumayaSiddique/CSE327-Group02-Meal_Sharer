3e5b4fb353e1d3933ba2393eb34ca77a355a8c1c
SumayaSiddique <73577002+SumayaSiddique@users.noreply.github.com>
2021-12-01 05:00:34 +0600

    Created profile screen UI

---
+++ meal_sharer/lib/Screens/Profile/profile_screen.dart
@@ -0,0 +1,189 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:meal_sharer/Screens/Welcome/components/welcome_background.dart';
+
+import '../../constants.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 18.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    const Text(
+                      "Profile",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w700,
+                        color: sPrimaryColor,
+                      ),
+                    ),
+                    const Spacer(),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.edit,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                Center(
+                  child: Image.asset("assets/images/Profile.png"),
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                const Text(
+                  "Name",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    color: sPrimaryColor,
+                  ),
+                ),
+                const SizedBox(height: 6),
+                Container(
+                  height: 40,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Center(
+                    child: Row(
+                      children: const [
+                        SizedBox(width: 7),
+                        Text(
+                          "Kakashi Hatake",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: sPrimaryColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                const Text(
+                  "Bio",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    color: sPrimaryColor,
+                  ),
+                ),
+                const SizedBox(height: 6),
+                Container(
+                  height: 120,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Center(
+                    child: Row(
+                      children: const [
+                        SizedBox(width: 7),
+                        Text(
+                          "I am Kakashi Hatake and am a shinobi of \nKonohagakure's Hatake clan. I am also \nknown as Kakashi of the Sharingan.\nI know the improtance of teamwork.",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: sPrimaryColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                const Text(
+                  "Email",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    color: sPrimaryColor,
+                  ),
+                ),
+                const SizedBox(height: 6),
+                Container(
+                  height: 40,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Center(
+                    child: Row(
+                      children: const [
+                        SizedBox(width: 7),
+                        Text(
+                          "KakashioftheSharingan@email.com",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: sPrimaryColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                const Text(
+                  "Contact Number",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    color: sPrimaryColor,
+                  ),
+                ),
+                const SizedBox(height: 6),
+                Container(
+                  height: 40,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Center(
+                    child: Row(
+                      children: const [
+                        SizedBox(width: 7),
+                        Text(
+                          "0123456789",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: sPrimaryColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
