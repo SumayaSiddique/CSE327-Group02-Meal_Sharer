65d2081560b1cb8ee18cc1ffb498d9437c591799
SumayaSiddique <73577002+SumayaSiddique@users.noreply.github.com>
2021-12-02 16:20:35 +0600

    Update forget_password_screen.dart

--- meal_sharer/lib/Screens/Forgot password/forget_password_screen.dart
+++ meal_sharer/lib/Screens/Forgot password/forget_password_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meal_sharer/Screens/Background/background_2.dart';
 import 'package:meal_sharer/Screens/Welcome/components/welcome_background.dart';
 import 'package:meal_sharer/constants.dart';
 
@@ -10,111 +11,118 @@ class ForgetPasswordScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: WelcomeBackground(
-        child: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 12,
-                ),
-                Image.asset("assets/images/forgot_password.png"),
-                const SizedBox(
-                  height: 12,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 18.0),
+      body: Background2(
+        child: SafeArea(
+          child: Stack(
+            children: [
+              Center(
+                child: SingleChildScrollView(
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      Text(
-                        "Reset Password",
-                        style: TextStyle(
-                          fontSize: 25,
-                          color: sPrimaryColor,
-                          fontFamily: 'SF Pro Rounded',
-                          fontWeight: FontWeight.w700,
+                    children: [
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      Image.asset("assets/images/forgot_password.png"),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 18.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            Text(
+                              "Reset Password",
+                              style: TextStyle(
+                                fontSize: 25,
+                                color: sPrimaryColor,
+                                fontFamily: 'SF Pro Rounded',
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 6,
+                            ),
+                            Opacity(
+                              opacity: 0.7,
+                              child: Text(
+                                "Enter the email associated with your account and we'll send an email with instructions to rest your password.",
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: sTextColor,
+                                  fontFamily: 'SF Pro Rounded',
+                                  fontWeight: FontWeight.w300,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                      SizedBox(
-                        height: 6,
+                      const SizedBox(
+                        height: 12,
                       ),
-                      Opacity(
-                        opacity: 0.7,
-                        child: Text(
-                          "Enter the email associated with your account and we'll send an email with instructions to rest your password.",
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: sTextColor,
-                            fontFamily: 'SF Pro Rounded',
-                            fontWeight: FontWeight.w300,
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 18.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 8.0),
+                            child: TextField(
+                              decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                labelText: "Email",
+                                suffixIcon: Icon(
+                                  Icons.mail,
+                                ),
+                              ),
+                              onChanged: (value) {
+                                // email = value;
+                              },
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 18,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                        child: SizedBox(
+                          width: double.infinity,
+                          height: 55,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              primary: sPrimaryColor,
+                              shape: const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(20.0),
+                                ),
+                              ),
+                            ),
+                            onPressed: () {
+                              // Get.to(() => const HomeScreen());
+                            },
+                            child: const Text(
+                              'Reset Password',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24,
+                                fontFamily: 'SF Pro Rounded',
+                              ),
+                            ),
                           ),
                         ),
                       ),
                     ],
                   ),
                 ),
-                const SizedBox(
-                  height: 12,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 18.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: TextField(
-                        decoration: const InputDecoration(
-                          border: InputBorder.none,
-                          labelText: "Email",
-                          suffixIcon: Icon(
-                            Icons.mail,
-                          ),
-                        ),
-                        onChanged: (value) {
-                          // email = value;
-                        },
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 18,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 32.0),
-                  child: SizedBox(
-                    width: double.infinity,
-                    height: 55,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: sPrimaryColor,
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(20.0),
-                          ),
-                        ),
-                      ),
-                      onPressed: () {
-                        // Get.to(() => const HomeScreen());
-                      },
-                      child: const Text(
-                        'Reset Password',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 24,
-                          fontFamily: 'SF Pro Rounded',
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
