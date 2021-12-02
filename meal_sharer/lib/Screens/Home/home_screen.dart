8b04414c91b7da5291025988adc8bf0d46eb4aba
SumayaSiddique <73577002+SumayaSiddique@users.noreply.github.com>
2021-12-02 16:20:40 +0600

    Update home_screen.dart

--- meal_sharer/lib/Screens/Home/home_screen.dart
+++ meal_sharer/lib/Screens/Home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
+import 'package:meal_sharer/Screens/Background/background_2.dart';
 import 'package:meal_sharer/Screens/Profile/profile_screen.dart';
 import 'package:meal_sharer/Screens/Welcome/components/welcome_background.dart';
 import 'package:meal_sharer/constants.dart';
@@ -16,84 +17,91 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: WelcomeBackground(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+      body: Background2(
+        child: SafeArea(
+          child: Stack(
             children: [
-              const Image(
-                image: AssetImage('assets/images/Homepage.png'),
-              ),
-              const SizedBox(
-                height: 22,
-              ),
-              const Text(
-                "Make good things happen",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 26,
-                ),
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: Get.width * 0.1),
-                child: const Text(
-                  "Meal sharer is the best place for donation whether you are an individual or group or maybe an organization.",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w400,
-                    fontSize: 16,
-                    letterSpacing: 1.2,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  SizedBox(
-                    height: 100,
-                    width: 100,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: const Text(
-                        "Donate\nFood",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: sBackgroundColor,
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        shape: const CircleBorder(),
-                        primary: sPrimaryColor,
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Image(
+                      image: AssetImage('assets/images/Homepage.png'),
+                    ),
+                    const SizedBox(
+                      height: 22,
+                    ),
+                    const Text(
+                      "Make good things happen",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 26,
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 100,
-                    width: 100,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Get.to(() => const ProfileScreen());
-                      },
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: Get.width * 0.1),
                       child: const Text(
-                        "Find\nDonated\nFoods",
-                        textAlign: TextAlign.center,
+                        "Meal sharer is the best place for donation whether you are an individual or group or maybe an organization.",
                         style: TextStyle(
-                          color: sBackgroundColor,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 16,
+                          letterSpacing: 1.2,
                         ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        shape: const CircleBorder(),
-                        primary: sPrimaryColor,
+                        textAlign: TextAlign.center,
                       ),
                     ),
-                  ),
-                ],
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        SizedBox(
+                          height: 100,
+                          width: 100,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: const Text(
+                              "Donate\nFood",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: sBackgroundColor,
+                              ),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              shape: const CircleBorder(),
+                              primary: sPrimaryColor,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 100,
+                          width: 100,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Get.to(() => const ProfileScreen());
+                            },
+                            child: const Text(
+                              "Find\nDonated\nFoods",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: sBackgroundColor,
+                              ),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              shape: const CircleBorder(),
+                              primary: sPrimaryColor,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
