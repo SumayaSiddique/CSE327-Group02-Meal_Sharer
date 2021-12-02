dcc768340f0badb47f0f48962917cdf57409944a
SumayaSiddique <73577002+SumayaSiddique@users.noreply.github.com>
2021-12-02 16:20:43 +0600

    Update login_screen.dart

--- meal_sharer/lib/Screens/Login/login_screen.dart
+++ meal_sharer/lib/Screens/Login/login_screen.dart
@@ -2,10 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
+import 'package:meal_sharer/Screens/Background/background_2.dart';
 import 'package:meal_sharer/Screens/Forgot%20password/forget_password_screen.dart';
 import 'package:meal_sharer/Screens/Home/home_screen.dart';
-
-import 'package:meal_sharer/Screens/Welcome/components/welcome_background.dart';
 import 'package:meal_sharer/constants.dart';
 
 class LogInScreen extends StatefulWidget {
@@ -22,172 +21,180 @@ class _LogInScreenState extends State<LogInScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: WelcomeBackground(
-        child: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 12,
-                ),
-                SvgPicture.asset('assets/icons/login.svg'),
-                const SizedBox(
-                  height: 12,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 18.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: TextField(
-                        decoration: const InputDecoration(
-                          border: InputBorder.none,
-                          labelText: "Email",
-                          suffixIcon: Icon(
-                            Icons.mail,
-                          ),
-                        ),
-                        onChanged: (value) {
-                          email = value;
-                        },
+      body: Background2(
+        child: SafeArea(
+          child: Stack(
+            children: [
+              Center(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        height: 12,
                       ),
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 12,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 18.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: TextField(
-                        obscureText: true,
-                        decoration: const InputDecoration(
-                          border: InputBorder.none,
-                          labelText: "Password",
-                          suffixIcon: Icon(
-                            Icons.password,
-                          ),
-                        ),
-                        onChanged: (value) {
-                          password = value;
-                        },
+                      SvgPicture.asset('assets/icons/login.svg'),
+                      const SizedBox(
+                        height: 12,
                       ),
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 18,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 32.0),
-                  child: SizedBox(
-                    width: double.infinity,
-                    height: 55,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: sPrimaryColor,
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(20.0),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 18.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(8),
                           ),
-                        ),
-                      ),
-                      onPressed: () {
-                        print(email + " and " + password);
-                        Get.to(() => const HomeScreen());
-                      },
-                      child: const Text(
-                        'Login',
-                        style: TextStyle(
-                          color: sBackgroundColor,
-                          fontSize: 24,
-                          fontFamily: 'SF Pro Rounded',
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 32.0),
-                  child: SizedBox(
-                    width: double.infinity,
-                    height: 55,
-                    child: TextButton(
-                      onPressed: () {
-                        Get.to(() => const ForgetPasswordScreen());
-                      },
-                      child: const Text(
-                        'forgot password?',
-                        style: TextStyle(
-                          color: sPrimaryColor,
-                          fontSize: 18,
-                          fontFamily: 'SF Pro Rounded',
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Divider(
-                    height: 1,
-                    thickness: 1,
-                  ),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 32.0),
-                  child: SizedBox(
-                    width: double.infinity,
-                    height: 50,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.black,
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(20.0),
-                          ),
-                        ),
-                      ),
-                      onPressed: () {},
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [
-                          Icon(
-                            FontAwesomeIcons.googlePlusG,
-                            color: Colors.redAccent,
-                          ),
-                          SizedBox(
-                            width: 30,
-                          ),
-                          Text(
-                            'Login with Google',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 18,
-                              fontWeight: FontWeight.w400,
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 8.0),
+                            child: TextField(
+                              decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                labelText: "Email",
+                                suffixIcon: Icon(
+                                  Icons.mail,
+                                ),
+                              ),
+                              onChanged: (value) {
+                                email = value;
+                              },
                             ),
                           ),
-                        ],
+                        ),
                       ),
-                    ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 18.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 8.0),
+                            child: TextField(
+                              obscureText: true,
+                              decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                labelText: "Password",
+                                suffixIcon: Icon(
+                                  Icons.password,
+                                ),
+                              ),
+                              onChanged: (value) {
+                                password = value;
+                              },
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 18,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                        child: SizedBox(
+                          width: double.infinity,
+                          height: 55,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              primary: sPrimaryColor,
+                              shape: const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(20.0),
+                                ),
+                              ),
+                            ),
+                            onPressed: () {
+                              print(email + " and " + password);
+                              Get.to(() => const HomeScreen());
+                            },
+                            child: const Text(
+                              'Login',
+                              style: TextStyle(
+                                color: sBackgroundColor,
+                                fontSize: 24,
+                                fontFamily: 'SF Pro Rounded',
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                        child: SizedBox(
+                          width: double.infinity,
+                          height: 55,
+                          child: TextButton(
+                            onPressed: () {
+                              Get.to(() => const ForgetPasswordScreen());
+                            },
+                            child: const Text(
+                              'forgot password?',
+                              style: TextStyle(
+                                color: sPrimaryColor,
+                                fontSize: 18,
+                                fontFamily: 'SF Pro Rounded',
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 16.0),
+                        child: Divider(
+                          height: 1,
+                          thickness: 1,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                        child: SizedBox(
+                          width: double.infinity,
+                          height: 50,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.black,
+                              shape: const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(20.0),
+                                ),
+                              ),
+                            ),
+                            onPressed: () {},
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const [
+                                Icon(
+                                  FontAwesomeIcons.googlePlusG,
+                                  color: Colors.redAccent,
+                                ),
+                                SizedBox(
+                                  width: 30,
+                                ),
+                                Text(
+                                  'Login with Google',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
