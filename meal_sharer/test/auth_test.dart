fc5d94b678ccb8f9625de3cdc02b0d11969d492c
Sumaya Siddique <sumaya.siddique@northsouth.edu>
2022-01-03 17:44:54 +0600

    Code Documentation added

--- meal_sharer/test/auth_test.dart
+++ meal_sharer/test/auth_test.dart
@@ -3,6 +3,7 @@ import 'package:meal_sharer/Screens/Login/login_screen.dart';
 import 'package:meal_sharer/Screens/Sign-up/sign_up_screen.dart';
 
 void main() {
+  // this is validating the email textformfield. if it's not empty then it will return null
   test('Empty Email Text form field Validator', () {
     var result = EmailFieldValidator.validate('');
     expect(result, 'Email can not be empty!');
@@ -11,6 +12,8 @@ void main() {
     var result = EmailFieldValidator.validate('sss.savant99@gmail.com');
     expect(result, null);
   });
+
+  // this is validating the password textformfield. if it's not empty then it will return null
   test('Empty Password Text form field Validator', () {
     var result = PasswordFieldValidator.validate('');
     expect(result, 'Password can not be empty!');
@@ -19,6 +22,9 @@ void main() {
     var result = PasswordFieldValidator.validate('1234');
     expect(result, null);
   });
+
+  // this is validating the full name textformfield. if it's not empty then it will return null
+
   test('Empty Name Text form field Validator', () {
     var result = NameFieldValidator.validate('');
     expect(result, 'Name can not be empty!');
@@ -27,6 +33,9 @@ void main() {
     var result = NameFieldValidator.validate('Test');
     expect(result, null);
   });
+
+  // this is validating the contact number textformfield. if it's not empty then it will return null
+
   test('Empty Contact Number Text form field Validator', () {
     var result = ContactNumberFieldValidator.validate('');
     expect(result, 'Number can not be empty!');
